0797da9c0a670cb2e3c575c0dccbc43e371fec1c
Waleed Tariq <dev.ratech@outlook.com>
2022-01-18 01:31:25 +0500

    add form

--- src/components/Welcome.jsx
+++ src/components/Welcome.jsx
@@ -1,7 +1,9 @@
+import React, { useContext } from 'react';
 import { AiFillPlayCircle } from 'react-icons/Ai';
 import { SiEthereum } from 'react-icons/si';
 import { BsInfoCircle } from 'react-icons/bs';
 
+import { TransactionContext } from '../context/TransactionContext'
 import { Loader } from './';
 
 const commonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
@@ -17,8 +19,17 @@ const Input = ({ placeholder, name, type, value, handleChange }) => (
 );
 
 const Welcome = () => {
-    const connectWallet = () => { };
-    const handleSubmit = () => { };
+    const { connectWallet, currentAccount, formData, handleChange, sendTransaction } = useContext(TransactionContext);
+
+    const handleSubmit = (e) => {
+        const { addressTo, amount, keyword, message } = formData;
+        e.preventDefault();
+        
+        if(!addressTo || !amount || !keyword || !message) return;
+
+        sendTransaction();
+
+    };
     return (
         <div className='flex w-full justify-center items-center'>
             <div className='flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4'>
@@ -29,13 +40,15 @@ const Welcome = () => {
                     <p className='text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base'>
                         Explore the crypto world. Buy and sell cryptocurrencies easily on Krypt
                     </p>
-                    <button
-                        type='button'
-                        onClick={connectWallet}
-                        className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
-                    >
-                        <p className='text-white text-base font-semibold'>Connect Wallet</p>
-                    </button>
+                    {!currentAccount && (
+                        <button
+                            type='button'
+                            onClick={connectWallet}
+                            className='flex flex-row justify-center items-center my-5 bg-[#2952e3] p-3 rounded-full cursor-pointer hover:bg-[#2546bd]'
+                        >
+                            <p className='text-white text-base font-semibold'>Connect Wallet</p>
+                        </button>
+                    )}
                     <div className='grid sm:grid-cols-3 grid-cols-2 w-full mt-10'>
                         <div className={`rounded-tl-2xl ${commonStyles}`}>
                             Reliability
@@ -75,14 +88,14 @@ const Welcome = () => {
                     </div>
 
                     <div className='p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism'>
-                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={() => { }} />
-                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={() => { }} />
-                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={() => { }} />
-                        <Input placeholder="Enter Message" name="message" type="text" handleChange={() => { }} />
+                        <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
+                        <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
+                        <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
+                        <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
 
                         <div className='h-[1px] w-full bg-gray-400 my-2' />
 
-                        {true ? (
+                        {false ? (
                             <Loader />
                         ) : (
                             <button
